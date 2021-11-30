92168c130d2f729a51025e5cfc6833ee474f70be
Sunny <sunnygb.8@gmail.com>
2021-11-30 10:20:24 +0000

    Create new component, import in new page

---
+++ vue_app_3/src/components/MyComp.vue
@@ -0,0 +1,6 @@
+<template>
+    <div id='mycomp'>
+        <h3>My Component</h3>
+        <h4>URL: src/components/MyComp.vue</h4>
+    </div>
+</template>
--- vue_app_3/src/views/MyPage.vue
+++ vue_app_3/src/views/MyPage.vue
@@ -2,5 +2,15 @@
     <div id='mypage'>
         <h1>My Own Page</h1>
         <h2>MyPage.vue in src/views/</h2>
+        <MyComp />
     </div>
 </template>
+
+<script>
+import MyComp from '@/components/MyComp'
+export default {
+    components: {
+        MyComp
+    }
+}
+</script>

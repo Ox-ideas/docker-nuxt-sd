21b1144140f83fda56a7e2dc0c42085572fa9fc7
Sunny <sunnygb.8@gmail.com>
2021-11-30 08:45:31 +0000

    Create new page, set route, and link

--- vue_app_3/src/router/index.js
+++ vue_app_3/src/router/index.js
@@ -14,7 +14,15 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/mypage',
+    name: 'My Page',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "mypage" */ '../views/MyPage.vue')
+  },
 ]
 
 const router = createRouter({
